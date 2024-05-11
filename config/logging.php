d57bfa200fd2c4873b92f7b78114231b46dca8ad
Ravshan Makharov <ravshan@makharov.pw>
2024-05-11 15:11:29 +0500

    Simple refactoring...

--- config/logging.php
+++ config/logging.php
@@ -132,8 +132,8 @@ return [
             'driver' => 'custom',
             'via' => TelegramLoggerFactory::class,
             'level' => env('LOG_LEVEL', 'debug'),
-            'chat_id' => env('TELEGRAM_CHAT_ID', ''),
-            'token' => env('TELEGRAM_TOKEN', ''),
+            'chat_id' => (int) env('TELEGRAM_CHAT_ID'),
+            'token' => env('TELEGRAM_TOKEN'),
         ]
     ],
 
